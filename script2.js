3f11bcd03c22da94fff97f014e26581310dc8527
T-Lucas43 <133705031+T-Lucas43@users.noreply.github.com>
2024-06-07 17:40:55 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previne o envio do formulário padrão
+    
+    // Aqui você pode adicionar código JavaScript para validar o formulário ou enviar os dados para o servidor, se necessário
+    // Por exemplo:
+    // var nome = document.getElementById("nome").value;
+    // var cpf = document.getElementById("cpf").value;
+    // var email = document.getElementById("email").value;
+    // ... Validação dos campos ...
+    // ... Envio dos dados para o servidor ...
+  });
+  
